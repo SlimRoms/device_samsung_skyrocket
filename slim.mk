1bcd6b2c513733e24352782f7571051d9ccbcdd9
prbassplayer <prbassplayer@gmail.com>
2013-04-16 22:44:30 +0200

    Changed release name
    Change-Id: I8d4b779ff190bdf5e6c6812b891cac9d823cc0ac

--- slim.mk
+++ slim.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/skyrocket/full_skyrocket.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := i727
+PRODUCT_RELEASE_NAME := skyrocket
 
 # Inherit some common slim stuff.
 $(call inherit-product, vendor/slim/config/gsm.mk)
@@ -21,6 +21,9 @@ $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 # Copy boot animation
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_NAME := slim_skyrocket
+PRODUCT_DEVICE := skyrocket
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.1.2/IMM76D/UCLI3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.1.2 IMM76D UCLI3 release-keys"
 
