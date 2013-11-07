21cd62fac27065a33792b5e7ac39926a2e797399
Josue Rivera <prbassplayer@gmail.com>
2013-11-07 02:45:26 +0100

    Skyrocket: Updte dependencies and cleanup mk files
    Change-Id: I55f45ca028b2cd57ccba47fd4415ffcd18a35065
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -1,11 +1,12 @@
-$(call inherit-product, device/samsung/skyrocket/full_skyrocket.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := skyrocket
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/skyrocket/full_skyrocket.mk)
+
+# Inherit some common slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit some common slim stuff.
 $(call inherit-product, vendor/slim/config/gsm.mk)
@@ -13,12 +14,13 @@ $(call inherit-product, vendor/slim/config/gsm.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
-# Inherit some common slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
-
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
+# Release name
+PRODUCT_RELEASE_NAME := skyrocket
+
+# Device identifier
 PRODUCT_NAME := slim_skyrocket
 PRODUCT_DEVICE := skyrocket
 
