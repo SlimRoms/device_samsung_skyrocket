69f098c991f3e244722972c8a8e52a4147800ff8
Dan Cartier <nospamdan11@gmail.com>
2015-02-18 18:11:39 +0100

    Revert "skyrocket: Add device resolution."
    Already defined in slim.mk.
    
    This reverts commit 8db2f95e5109759f01275f05fecb9c34f7b00b75.
    
    Change-Id: I908a5ec80ceeef3d982795684c2ec777249e430c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,9 +22,6 @@
 TARGET_BOARD_INFO_FILE ?= device/samsung/skyrocket/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := SGH-I727,skyrocket
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/skyrocket/bluetooth
-
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_skyrocket_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8660
@@ -39,5 +36,5 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 627048448
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 20044333056
 TARGET_USERIMAGES_USE_EXT4 := true
 
-# Resolution
-DEVICE_RESOLUTION := 480x800
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/skyrocket/bluetooth
