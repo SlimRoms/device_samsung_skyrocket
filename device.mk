155d76609cd201ef11a78ef9a94a7fa8c7817baf
Dan Cartier <nospamdan1@gmail.com>
2015-10-27 07:08:45 +0100

    Skyrocket: Move some common stuff to celox-common (1/3)
     * Move out kernel common configs
     * Move out partition information
     * Move out device resolution
     * Move out F2FS package call
     * Move out bluetooth firmware file
     * Move out common system props
    
    Credit to Josue Rivera <prbassplayer@gmail.com>
    
    Change-Id: Iea0e3f62d806d8f25f387a151eb80c450c0109d3

--- device.mk
+++ device.mk
@@ -20,19 +20,9 @@ $(call inherit-product-if-exists, vendor/samsung/skyrocket/skyrocket-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# F2FS
-PRODUCT_PACKAGES += \
-    mkfs.f2fs \
-    fsck.f2fs \
-    fibmap.f2fs
-
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ramdisk/init.qcom.usb.rc:root/init.qcom.usb.rc
-
-# Bluetooth
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/firmware/bcm4330B1.hcd:system/etc/firmware/bcm4330B1.hcd
 
 # Inherit from celox-common
 $(call inherit-product, device/samsung/celox-common/celox-common.mk)
